9e44573663e4737d8039ff0ed07d22f6dbe4a099
mavion7333 <sameerchoudhary7333@gmail.com>
2026-06-29 18:40:04 +0530

    updated the score and input = userName

---
+++ random-Value-Array/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    question: "What is 2 + 2?",
+    ans: 4,
+    options: [2, 4, 6, 8],
+  },
+  {
+    question: "What is 2 * 2 * 0 * 2?",
+    ans: 0,
+    options: [2, 0, 8, 222],
+  },
+  {
+    question: "What is 3 * 3?",
+    ans: 9,
+    options: [3, 9, 33, 6],
+  },
+  {
+    question: "Which is the biggest planet in solar system?",
+    ans: "Jupiter",
+    options: ["Jupiter", "Saturn", "Earth", "Mercury"],
+  },
+  {
+    question: "Who is the President of India?",
+    ans: "Draupadi Murmu",
+    options: ["Melody", "Trump", "Modi", "Draupadi Murmu"],
+  },
+];
---
+++ random-Value-Array/randomValueFromArray.js
@@ -0,0 +1,127 @@
+import { data } from "./data.js";
+
+const questionDiv = document.querySelector("#question");
+const options = document.querySelectorAll("#options button");
+const timerDiv = document.querySelector("#timer");
+const quizDiv = document.querySelector("#quiz");
+const scoreDiv = document.querySelector("#score");
+const scoreH1 = document.querySelector("#score h1");
+const message = document.querySelector("#message");
+const startQuiz = document.querySelector("#startquiz");
+const userName = document.querySelector("#username");
+const startBtn = document.querySelector("#startbtn");
+
+let questionNumber = 0;
+let time = 5;
+let score = 0;
+let order = [];
+order = getRandomOrder();
+console.log(order);
+
+data.sort(() => Math.random() - 0.5);
+
+function startTimer() {
+  const interval = setInterval(() => {
+
+    if (time === 0) {
+
+      if (questionNumber === data.length - 1) {
+
+        clearInterval(interval);
+        quizDiv.style.display = "none";
+        scoreDiv.style.display = "flex";
+
+        displayScore();
+      } else {
+        questionNumber++;
+        time = 5;
+        timerDiv.innerText = time;
+        printQuestionAndAns();
+      }
+    } else {
+      time--;
+      timerDiv.innerText = time;
+    }
+  }, 1000);
+}
+
+
+startBtn.addEventListener("click", function () {
+  if (userName.value.trim() === "") {
+    alert("Please enter your name!");
+    return;
+  }
+  startQuiz.style.display = "none";
+  quizDiv.style.display = "block";
+
+  printQuestionAndAns();
+  startTimer();
+});
+//////////////////////////////////////////////////////////////
+
+// THIS SHOWS THE SCORE AT THE END
+function displayScore() {
+
+  scoreH1.innerHTML = `Congratulations ${username.value}<br><br>
+        You scored ${score} out of ${data.length}`;
+
+}
+//RESET BACKGROUND COLOR ON OPTIONS
+function printQuestionAndAns() {
+
+  questionDiv.innerText = data[questionNumber].question;
+  message.innerText = "";
+
+  for (let i = 0; i < options.length; i++) {
+    options[i].innerText = data[questionNumber].options[i];
+
+    options[i].classList.remove("correctAnswer");
+    options[i].classList.remove("inCorrectAnswer");
+    options[i].disabled = false;
+  }
+}
+
+
+// //WHEN USER CLICKS AN OPTION
+for (let i = 0; i < options.length; i++) {
+
+  options[i].addEventListener("click", function (e) {
+    for (let j = 0; j < options.length; j++) {
+      options[j].disabled = true;
+    }
+
+    if (e.target.innerText == data[questionNumber].ans) {
+      score++;
+
+      e.target.classList.add("correctAnswer");
+      message.innerText = "✅ Correct Answer!";
+      message.style.color = "green";
+
+    } else {
+      e.target.classList.add("inCorrectAnswer");
+      message.innerText = "❌ Wrong Answer!";
+      message.style.color = "red";
+
+      for (let j = 0; j < options.length; j++) {
+        if (options[j].innerText == data[questionNumber].ans) {
+          options[j].classList.add("correctAnswer");
+        }
+      }
+    }
+  });
+}
+function getRandomOrder() {
+  for (let i = 0; i < data.length; i++) {
+    const randomNumber = getDistinctOrder();
+    order.push(randomNumber)
+  }
+  return order
+}
+
+function getDistinctOrder() {
+  const randomValue = Math.floor(Math.random() * data.length);
+  if (order.includes(randomValue)) return getDistinctOrder();
+  else {
+    return randomValue;
+  }
+}
